7343858642e7fb95a6ccb00154d5fb94dd8293a1
Natalia <alimadeoliveiranatalia@gmail.com>
2022-02-15 19:21:03 -0300

    Utilisando SRP

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 
 const categoriesRoutes = Router();
@@ -9,12 +10,10 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req,res)=> {
     const {name,description} = req.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-    if(categoryAlreadyExists){
-        return res.status(400).json({error:"Category Already Exists!"});
-    }
-    categoriesRepository.create({name, description});
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    
+    createCategoryService.execute({name, description});
+    
     return res.status(201).send();
 
 });
