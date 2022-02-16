3f049e5ab8a1eb9cff636c0f951094c520bda32b
Natalia <alimadeoliveiranatalia@gmail.com>
2022-02-16 15:53:07 -0300

    Enxugando Rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,19 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCase/createCategory";
+import { listCategoriesController } from "../modules/cars/useCase/listCategory";
+
 
 
 
 const categoriesRoutes = Router();
 
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req,res)=> {
     return createCategoryController.handle(req,res);
-
 });
 categoriesRoutes.get("/",(req,res)=>{
-    const all = categoriesRepository.list();
-    return res.json(all);
+    return listCategoriesController.handle(req,res);
 });
 
 export { categoriesRoutes };
