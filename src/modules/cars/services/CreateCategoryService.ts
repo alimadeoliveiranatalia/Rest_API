4ba18a224c573dab099b059d6a1cc78f5dda0a2f
Natalia <alimadeoliveiranatalia@gmail.com>
2022-02-15 22:05:54 -0300

    Corrigindo direcionamento de repositorio

--- src/modules/cars/services/CreateCategoryService.ts
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { ICategoriesRepository } from "../modules/cars/model/repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest{
     name: string;
