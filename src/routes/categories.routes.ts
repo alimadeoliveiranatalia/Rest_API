29450778f877864cd06373b1be88aba4330f0b86
Natalia <alimadeoliveiranatalia@gmail.com>
2022-02-17 15:25:37 -0300

    Inserindo leituras de arquivos CSV

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,24 @@
-import { Router } from "express";
-import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
+import { response, Router } from "express";
+import multer, { Multer } from "multer";
 import { createCategoryController } from "../modules/cars/useCase/createCategory";
 import { listCategoriesController } from "../modules/cars/useCase/listCategory";
 
-
-
-
 const categoriesRoutes = Router();
 
+const upload = multer({
+    dest:"./tmp",
+});
 
 categoriesRoutes.post("/", (req,res)=> {
     return createCategoryController.handle(req,res);
 });
 categoriesRoutes.get("/",(req,res)=>{
     return listCategoriesController.handle(req,res);
+});
+categoriesRoutes.post("/import",upload.single("file"),(req,res)=>{
+    const {file} = req;
+    console.log(file);
+    return res.send();
 });
 
 export { categoriesRoutes };
