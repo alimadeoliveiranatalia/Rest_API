01804a1ed738237f3ff9bbf0de69e6e1ce129de6
Natalia <alimadeoliveiranatalia@gmail.com>
2022-02-18 13:32:35 -0300

    Removendo os arquivos de upload

--- src/modules/cars/useCase/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCase/importCategory/ImportCategoryUseCase.ts
@@ -17,7 +17,6 @@ class ImportCategoryUseCase {
             const parseFile = parse();
             stream.pipe(parseFile);
             parseFile.on("data", async (line) =>{
-                //["name", "description"]
                 const [name, description] = line;
                 categories.push({
                     name,
@@ -25,6 +24,7 @@ class ImportCategoryUseCase {
                 });
             })
             .on("end",() => {
+                fs.promises.unlink(file.path);
                 resolve(categories);
             })
             .on("error",(err) => {
