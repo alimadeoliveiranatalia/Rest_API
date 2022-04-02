ff962b72186a825603f920086781ea912fa85c7d
Natalia <alimadeoliveiranatalia@gmail.com>
2022-04-02 01:51:39 -0300

    Add interfaces specification

--- src/modules/cars/repositories/ISpecificatiosRepository.ts
+++ src/modules/cars/repositories/ISpecificatiosRepository.ts
@@ -8,5 +8,7 @@ interface ISpecificationsRepository{
     create({description, name}: ICreateSpecidicationDTO): Promise<void>;
     
     findByName(name: string): Promise<Specification>;
+
+    list():Promise<Specification[]>
 }
 export {ICreateSpecidicationDTO,ISpecificationsRepository}
