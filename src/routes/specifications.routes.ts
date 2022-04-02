83b4cc59228d3969909421b605fc1db52481ff67
Natalia <alimadeoliveiranatalia@gmail.com>
2022-04-02 01:46:57 -0300

    Add route list specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { CreateSpecificationController } from "../modules/cars/useCase/createSpecification/CreateSpecificationController";
+import { ListSpecificationsController } from "../modules/cars/useCase/listSpecification/ListSpecificationController";
 
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+const listSpecificationsController = new ListSpecificationsController();
+
 specificationsRoutes.post("/", createSpecificationController.handle);
+
+specificationsRoutes.get("/", listSpecificationsController.handle);
 
 
 export {specificationsRoutes}
