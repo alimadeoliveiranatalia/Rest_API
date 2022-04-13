72b869e47cb75f021b54bc569f4c77caf5b0cab6
Natalia <alimadeoliveiranatalia@gmail.com>
2022-04-13 04:14:33 -0300

    Implementando test unitarios

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserCase.spec.ts
@@ -0,0 +1,60 @@
+import { AppError } from "../../../../erros/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UserRepositoryInMemory } from "../../repositories/in-memory/UserRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UserRepositoryInMemory;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+describe("Authenticate User", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UserRepositoryInMemory();
+        authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+        createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    });
+    it("should be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            driver_license:"000123",
+            email:"user@test.com",
+            password:"1234",
+            name:"User Test"
+        };
+        await createUserUseCase.execute(user);
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password
+        });
+
+        expect(result).toHaveProperty("token");
+
+    });
+
+    it("should not be able to authenticate an nonexistent user", async () => {
+        expect(async () => {
+            await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password:"1234",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to authenticate with incorrect password", () => {
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license: "9999",
+                email: "user@user.com",
+                password: "1234",
+                name: "User Test Error",
+            }
+            await createUserUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "incorrectPassword"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
