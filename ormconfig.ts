9b9b5a1d50b15c93a314ff7bf841267d71e6ef50
Natalia <alimadeoliveiranatalia@gmail.com>
2022-05-26 15:06:37 -0300

    implementando secret manager

---
+++ ormconfig.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+export default {
+    "type": "postgres",
+    "port": process.env.port,
+    "host": process.env.host,
+    "username": process.env.username,
+    "password": process.env.password,
+    "database": "cars",
+    "migrations": ["./src/shared/infra/typeorm/migrations/*.ts"],
+    "entities": ["./src/modules/**/entities/*.ts"],
+    "cli": {
+        "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    }
+}
