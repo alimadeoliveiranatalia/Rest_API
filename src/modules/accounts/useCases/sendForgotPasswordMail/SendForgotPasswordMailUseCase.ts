e97a1ef882b0b00aad84ce3a84d85d7bcec72dce
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-01 20:29:11 -0300

    Estilhização do sendMail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,4 +1,5 @@
 import { v4 as uuid } from 'uuid';
+import { resolve } from "path";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { AppError } from "@shared/erros/AppError";
@@ -21,6 +22,15 @@ class SendForgotPasswordMailUseCase {
     async execute(email: string) : Promise<void> {
         const user = await this.usersRepository.findByEmail(email);
 
+        const templatePath = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "emails",
+            "forgotPassword.hbs"
+        );
+
         if(!user){
             throw new AppError("User does not exists!");
         }
@@ -35,10 +45,16 @@ class SendForgotPasswordMailUseCase {
             expires_date
         });
 
+        const variables = {
+            name: user.name,
+            link: `${ process.env.FORGOT_MAIL_URL }${ token }`
+        }
+
         await this.mailProvider.sendMail(
             email,
             "Recuperação de senha",
-            `O link para o reset é ${ token }`
+            variables,
+            templatePath
         );
     }
 }
