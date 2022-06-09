f630e420befce355c80c1ed0f72721db301ea742
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-08 22:29:13 -0300

    S3 funcionando

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 
@@ -17,7 +18,11 @@ container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
 );
+const diskStorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider
+};
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
-    LocalStorageProvider
+    diskStorage[process.env.disk]
 );
