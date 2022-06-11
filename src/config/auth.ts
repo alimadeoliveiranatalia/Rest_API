dada09431b50f4fc315d681704a80eceaa9bf39b
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-11 03:42:55 -0300

    vizibilidade auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "85226afac4aae2ac1beed64eee170b8c",
+    expires_in_token: "15m",
+    secret_refresh_token: "6a79a6fd66666296872d496020387c07",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30
+}
