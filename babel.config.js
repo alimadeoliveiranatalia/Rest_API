1239e0c1231cc356ef556305e5ef9d03f10633ce
Natalia <alimadeoliveiranatalia@gmail.com>
2022-06-11 02:36:40 -0300

    Config babel

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    presets: [
+        ["@babel/preset-env", { target: { node: "current"}}],
+        "@babel/preset-typescript"
+    ],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                alias: {
+                    "@modules":["./src/modules"],
+                    "@config":["./src/config"],
+                    "@shared":["./src/shared"],
+                    "@erros":["./src/shared/erros"],
+                    "@utils":["./src/utils"]
+                }
+            }
+        ]
+    ]
+}
